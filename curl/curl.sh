d51f9e8aba563740d2fb04caa4b3624190dd35e6
Monica Whalen <mwhalen805@gmail.com>
2026-05-08 14:20:53 -0400

    update curl example for new requirements

--- curl/curl.sh
+++ curl/curl.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 export API_KEY=""
 export API_BASE=""
-export DEPLOYMENT_ID="" #model name
+export API_MODEL=""
 
-curl $API_BASE"/openai/deployments/"$DEPLOYMENT_ID"/chat/completions?api-version="$API_VERSION \
-  -H "Content-Type: application/json" \
-  -H "OpenAI-Organization: $ORGANIZATION" \
-  -H "api-key: $API_KEY" \
-  -d '{
-  "model": "gpt-4.1",
-  "messages": [{"role":"system","content":"You are a helpful bot"},{"role":"user","content":"What is 2+2"}],
-  "temperature": 0,
-  "frequency_penalty":0,
-  "top_p":0.95
-}'
+curl $API_BASE"/responses" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $API_KEY" \
+-d "{
+\"model\": \"$API_MODEL\",
+\"input\": \"2+2=?\"
+}"
